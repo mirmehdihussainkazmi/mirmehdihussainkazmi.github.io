231ab750c2ab00e94d25f66de5693584ffcbf729
mirmehdihussainkazmi <115344135+mirmehdihussainkazmi@users.noreply.github.com>
2022-10-24 19:40:56 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var names=new Array();
 names[0]="mahdi"; names[1]="Yaakov"; names[2]="John"; names[3]="Jen"; names[4]="Jason"; names[5]="Paul"; names[6]="Frank"; names[7]="Larry"; names[8]="Paula"; names[9]="Laura";
+
 for (var i = 0; i < names.length; i++) {
 if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
 console.log("Goodbye "+ names[i])
